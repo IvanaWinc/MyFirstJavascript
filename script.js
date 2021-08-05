28669256f18b1641442d94fd636705adcf9ea5e5
Ivana <79462114+IvanaWinc@users.noreply.github.com>
2021-08-05 05:53:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Hello Winc Academy');
+console.log('Hello ivana');
+console.log(4 + 4);
+console.log(2 + 9);
+console.log(6 - 3);
+console.log(5 * 6);
+console.log(8 / 2);
+console.log(10 % 2);
+console.log(typeof 33);
+console.log(typeof "33");
